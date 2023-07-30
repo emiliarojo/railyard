d707a219143e26bdcef3aea2f9ab868aed07c195
Gabriel Martinez <ghmartinez96@hotmail.com>
2023-07-30 21:34:14 +0200

    Added link to chats#index and configured the pg search for projects.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,11 @@
 class Project < ApplicationRecord
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_description,
+  against: [ :name, :description ],
+  using: {
+    tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  }
+
   belongs_to :user
   has_many :requests, dependent: :destroy
   has_many :project_skills, dependent: :destroy
