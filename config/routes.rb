6105f866307db196e97b67367bedf614719aafd3
simon hu <simon.hu.1999@gmail.com>
2023-07-30 12:31:33 +0200

    Create route, action and view for projects#show and add style

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :users, only: [:show, :edit, :update]
-  get "/projects/index", to: "projects#index", as: "projects_index"
-  # resources :projects
+  resources :projects, only: [:index, :show]
   # get 'search', to: 'projects#index'
 end
