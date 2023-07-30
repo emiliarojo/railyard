b4fd722568792f3bf394172f2b25fdeb38519776
Gabriel Martinez <ghmartinez96@hotmail.com>
2023-07-30 13:41:21 +0200

    Half way of implementing omniauth and creation of user from it.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root "pages#home"
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  # devise_scope :user do
+  #   delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  # end
 end
