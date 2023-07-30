b7e11834c40e71df26220a843f1f5f9ea62e96fc
Simon <130766778+Spicysimonroll@users.noreply.github.com>
2023-07-30 10:28:37 +0200

    Revert "Projects index"

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  root 'pages#home'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  root 'pages#home'
   resources :users, only: [:show, :edit, :update]
-  get "/projects/index", to: "projects#index", as: "projects_index"
   # resources :projects
   # get 'search', to: 'projects#index'
 end
